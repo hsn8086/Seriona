27bbe8e496853ce349e4df66fc0e8d8b631d582d
kaizen857 <3064561709@qq.com>
2026-06-13 08:47:08 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed, &app, []()
+                     { QCoreApplication::exit(-1); }, Qt::QueuedConnection);
+    engine.loadFromModule("Seriona", "Main");
+
+    return QCoreApplication::exec();
+}
